4052953e2b362a9519debe99cdbfd38394690df9
H1net <ben@freshdevs.com>
2012-12-03 16:08:14 +0300

    wc add buttons to add/remove watching shows

--- src/coffee/tvShowManage.coffee
+++ src/coffee/tvShowManage.coffee
@@ -1,1 +1,12 @@
 jQuery -> 
+  content = $('#tvShows')
+  #container = content.find('.container')
+
+  tags = [
+      '.sales-focus'
+      '.service-focus'
+      '.other-focus'
+  ]
+
+  $.each content.find('.tvShowRemoveWatch'), (button) ->
+      alert('found'+button.attr(id))
--- web-app/js/tvShowManage.js
+++ web-app/js/tvShowManage.js
@@ -1,3 +1,10 @@
 (function() {
-  jQuery(function() {});
+  jQuery(function() {
+    var content, tags;
+    content = $('#tvShows');
+    tags = ['.sales-focus', '.service-focus', '.other-focus'];
+    return $.each(content.find('.tvShowRemoveWatch'), function(button) {
+      return alert('found' + button.attr(id));
+    });
+  });
 }).call(this);
